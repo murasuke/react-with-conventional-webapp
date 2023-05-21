c0f3d1949d15318a9a50dabc60ec2d5e942bf4e8
tkyk.niimura <tkyk.niimura@gmai.com>
2023-05-21 22:27:00 +0900

    rm dist

--- dist/Counter.d.ts
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react';
-type propType = {
-    initVal?: number;
-};
-declare const Counter: FC<propType>;
-export default Counter;
--- dist/lib.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as React } from 'react';
-export { default as ReactDOM } from 'react-dom';
-export { default as ReactDOMClient } from 'react-dom/client';
-export { default as Counter } from './Counter';
